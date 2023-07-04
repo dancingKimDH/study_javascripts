9442a48aba9d0688119459d6ebadd70b4fda843f
dancingkimDH <kimdonghyun0701@gmail.com>
2023-07-04 17:29:29 +0900

    update

---
+++ docs/browser_js/js/arrays.js
@@ -0,0 +1,20 @@
+// Array 초기화
+let init_array = new Array();
+
+// let init_array = [];도 초기화
+
+let fruits = ["apple", "banana", "orange", "mellon"];
+
+// fruits.push('grape');
+// 5
+// fruits.length
+// 5
+// fruits.indexOf('banana')
+// 1
+// fruits.join(', and ')
+// 'apple, and banana, and orange, and mellon, and grape'
+
+let fruits_second = ["berry", "strawberry"]
+
+// fruits와 fruits_second 붙이기 (List와 List)
+let fruits_concat = fruits.concat(fruits_second);
