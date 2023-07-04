1e0902bfbb17413981177d180eebf624465253e0
dancingkimDH <kimdonghyun0701@gmail.com>
2023-07-04 17:40:38 +0900

    update

--- docs/browser_js/js/carInforslist.js
+++ docs/browser_js/js/carInforslist.js
@@ -1,10 +1,4 @@
 
-
-
-
-
-
-
 let car_rows = [{"YEAR":"2020","CAR_NAME":"소나타","CAR_INFOR_ID":"CI001","COMPANY_ID":"C001"},
 {"YEAR":"2021","CAR_NAME":"코나","CAR_INFOR_ID":"CI002","COMPANY_ID":"C002"},
 {"YEAR":"2019","CAR_NAME":"SM6","CAR_INFOR_ID":"CI003","COMPANY_ID":"C003"},
@@ -13,7 +7,7 @@ let car_rows = [{"YEAR":"2020","CAR_NAME":"소나타","CAR_INFOR_ID":"CI001","CO
 
 let car = ``;
 let outHtml = ``;
-let carArray = new Array();
+
 
 for (let car of car_rows) {
     outHtml = `${outHtml}<tr> <td> ${car.YEAR} </td>
